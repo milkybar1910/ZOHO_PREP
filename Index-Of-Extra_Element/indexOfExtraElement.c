1ac5f7049512af5e45b0386c7838b9487f3b15b5
milkybar1910 <sivashankar1326@gmail.com>
2021-04-15 21:43:58 +0530

    EDGE CASES ARE HANDLED

--- Index-Of-Extra_Element/indexOfExtraElement.c
+++ Index-Of-Extra_Element/indexOfExtraElement.c
@@ -1,17 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
     
-
 int findIndex(int arr1[],int arr2[],int n){
+    int first = 0,last = n-2;
+
+    if(arr1[first] != arr2[first]){
+        return 0;
+    }
+    if(arr1[n-1]!=arr2[n-2]){
+        return n-1;
+    }
+    int mid;
     int index;
-    int left = 0 ,right =n-2;
-    while(left<=right){
-        int mid = (left+right)/2;
-        if(arr1[mid] == arr2[mid])
-            left = mid+1;
-        else{
+
+    
+    while(first<=last){
+        mid=(first+last )/2;
+        // printf("%d",mid);
+        if(arr1[mid]!=arr2[mid]){
             index = mid;
-            right = mid-1;
+            last = mid-1;
+        }
+        else{
+            first = mid+1;
         }
     }
     return index;
@@ -26,3 +37,5 @@ int main()
     printf("%d",index);
     return 0;
 }
+
+
